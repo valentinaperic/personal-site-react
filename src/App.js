d3adb46a5d54bf062e9e228000eb9f9360e4c3d1
valentinaperic <valentinaperic24@gmail.com>
2018-08-11 19:29:04 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ const About = () => (
     Hi my name is Valentina! I am a software engineer, community tech leader, and ice cream enthusiast.
     I currently work at ITX building web apps with my team. I also co-lead Girl Develop It - Rochester with my best friend Kristen.
      When I am not coding, I am either attending tech events, playing video games, or playing the violin in my community orchestra.
-hey test hey test
      </p>
   </div>
 );
