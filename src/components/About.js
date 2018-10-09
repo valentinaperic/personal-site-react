d382c023e32262140e493f86b5585d35984cbb17
valentinaperic <valentinaperic24@gmail.com>
2018-10-09 19:15:34 -0400

    updated the tabs in experience

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
 import germanyView from '../images/germany-view.jpg'
 
 const About = () => (
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom";
 
 const Experience = () => (
   <div className="experience-content">
       <Router>
         <div>
-          <Route path="/Experience" component={ExperienceContent} />
-          <Route path="/Experience/Tab1" component={Test} />
-          <Route path="/Experience/Tab2" component={Test1} />
-        </div>
+        <ExperienceContent />
+          <Route exact path="/Experience/Tab1" component={Test} />
+          <Route exact path="/Experience/Tab2" component={Test1} />
+          </div>
       </Router>
   </div>
-
-
 );
 
 const ExperienceContent = () => (
   <div className="navigation-bar">
     <ul className="navigation-link-list">
-      <li><Link exact to="/">Main Tab</Link></li>
-      <li><Link to="/Experience/Tab1" activeClassName="active">Tab</Link></li>
-      <li><Link to="/Experience/Tab2" activeClassName="active">Tab</Link></li>
-      <li><Link to="/tab-3">Tab</Link></li>
+      <li><NavLink to="/Experience/Tab1" activeClassName="selected">Tab</NavLink></li>
+      <li><NavLink to="/Experience/Tab2" activeClassName="selected">Tab</NavLink></li>
     </ul>
   </div>
 );
@@ -37,7 +32,5 @@ const Test1 = () => (
   <p>tab 2</p>
 
 );
-
-
 
 export default Experience;
